b3604464a195651d414e4a22239282d380c9e135
cameronTheBrazen <Cameron.Kingsley@my.tccd.edu>
2023-08-22 21:18:41 -0500

    Add self practice folder

---
+++ self-practice/Hello-World/Hello-World/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace HelloWorld
+{
+    internal class Program
+    { 
+        static void Main(string[]args)
+        { 
+
+
+    Console.WriteLine(" Hello World");
+    Console.WriteLine(" What is your name?");
+    string name = Console.ReadLine();
+    Console.WriteLine("your name is " + name + ".");
+            Console.WriteLine(" How old are you");
+            int age = Console.Read();
+            Console.WriteLine("Your name is "+ name + ", and your age is " + age + ".");
+
+
+        }
+        }
+}
