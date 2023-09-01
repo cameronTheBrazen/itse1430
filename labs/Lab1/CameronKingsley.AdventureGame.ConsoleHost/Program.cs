6571e6092907535d9b7e2bb507c7ed4767991799
cameronTheBrazen <Cameron.Kingsley@my.tccd.edu>
2023-08-31 19:56:39 -0500

    Initial commit

---
+++ labs/Lab1/CameronKingsley.AdventureGame.ConsoleHost/Program.cs
@@ -0,0 +1,37 @@
+﻿//     Cameron Kingsley         //
+//      August 31, 2023         //
+//   ITSE 1430 Adventure Game   //
+namespace GameField
+{
+    class Program
+    {
+        public void intro ()
+        {
+            Console.WriteLine("You awaken on the floor of a dark room.");
+            Console.WriteLine("As your vision begins to clear, yout notice"
+
+
+        }
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
