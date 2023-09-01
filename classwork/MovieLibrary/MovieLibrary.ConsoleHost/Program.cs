ed2d5040c27ab475836b0789480ed5ebaae97352
cameronTheBrazen <Cameron.Kingsley@my.tccd.edu>
2023-08-31 21:36:08 -0500

    added functions/refactor 1

--- classwork/MovieLibrary/MovieLibrary.ConsoleHost/Program.cs
+++ classwork/MovieLibrary/MovieLibrary.ConsoleHost/Program.cs
@@ -6,56 +6,86 @@
 /// in color?
 /// add edit view delete
 
-namespace ConsoleHost 
+
+
+
+
+string title = "";
+string genre = "";
+string description = "";
+int rating = 0;
+bool inColor = true;
+//in minutes
+int length = 0;
+decimal budget = 0;
+int releaseYear = 1900;
+string message = "message";
+
+GetMovie();
+DisplayMovie();
+
+
+
+void GetMovie ()
 {
-    class Program {
-
-        string title="";
-        string genre = "";
-        string description = "";
-        string rating = "";
-        bool inColor = true;
-        //in minutes
-        int length = 0;
-        decimal budget = 0;
-        int releaseYear = 1900;
-
-        public void Main ()
-        {
-            Console.WriteLine("enter a title:");
-            title = Console.ReadLine();
-            Console.WriteLine("enter a genre:");
-            genre = Console.ReadLine();
-            Console.WriteLine("enter a description:");
-            description = Console.ReadLine();
-            Console.WriteLine("enter a rating:");
-            rating = Console.ReadLine();
-            Console.WriteLine("enter the run length in minutes");
-            string lengthString  = Console.ReadLine();
+    title = ReadString("enter a title: ",true);
+    genre=ReadString("enter a genre: ",true);
+    description=ReadString("enter a description: ",false);
+    rating= ReadInt("enter a rating: ");
+    length= ReadInt("enter the length: ");
 
 
-
-
-
-            Console.WriteLine(title);
-            Console.WriteLine(genre);
-            Console.WriteLine(description);
-            Console.WriteLine(rating);
-            Console.WriteLine(lengthString);
-            Console.WriteLine(budget);
-            Console.WriteLine(inColor);
-            Console.WriteLine(releaseYear);
-        }
-
-
-
-
-
-
-
-
-
-    
-    }
 
 }
+
+int ReadInt (string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result;
+    if (Int32.TryParse(value,out result))
+        return result;
+    return 0;
+
+}
+
+
+string ReadString( string message, bool isRequired )
+{
+
+
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+
+    if (!isRequired)
+        return value;
+    return value;
+}
+
+void DisplayMovie ()
+{
+    Console.WriteLine(title);
+    Console.WriteLine(genre);
+    Console.WriteLine(description);
+    Console.WriteLine(rating);
+    Console.WriteLine(length);
+    Console.WriteLine(budget);
+    Console.WriteLine(inColor);
+    Console.WriteLine(releaseYear);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
