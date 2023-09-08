932a93919fe78721d5b91b4d14f7851683df85fc
Cameron Kingsley <freakycdk@gmail.com>
2023-09-08 13:11:14 -0500

    neew functions, increasing detail

--- labs/Lab1/CameronKingsley.AdventureGame.ConsoleHost/program2.cs
+++ labs/Lab1/CameronKingsley.AdventureGame.ConsoleHost/program2.cs
@@ -1,7 +1,9 @@
 ﻿bool isPlaying = true;
+int adjustedlocation=2;
 int roomNumber = 2;
 int lastRoom=2;
-
+int currentX=2;
+int currentY = 1;
 
 int calcValidation ()
 {
@@ -11,16 +13,45 @@ int calcValidation ()
     { 
         InvalidChoice();
         roomNumber= lastRoom;
+        roomRefactor();
         return roomNumber;
     }else if (roomNumber== 8|| roomNumber>=12) {
             InvalidChoice();
             roomNumber= lastRoom;
+        roomRefactor();
             return roomNumber;
-        }else
+       
+    }else 
     {
+        roomRefactor();
         return roomNumber;
     }
         
+
+}
+void roomRefactor () {
+    if (roomNumber>4&&roomNumber<8)
+    {   
+
+        adjustedlocation = roomNumber-1;
+        currentX = roomNumber-4;
+        currentY=roomNumber -3;
+    } else if(roomNumber>8&& roomNumber <12) { 
+        adjustedlocation = roomNumber-2;
+        currentX = roomNumber-8;
+        currentY=roomNumber -6;
+    } else
+    {
+        adjustedlocation=roomNumber;
+        currentX = roomNumber;
+        currentY=1;
+    }
+}
+void displayCoordinate () {
+
+    Console.WriteLine("--------------------------------------------------------");
+    Console.WriteLine( "N" +currentX+ ", E"+ currentY);
+    
 
 }
 void InvalidChoice () {
@@ -28,13 +59,16 @@ void InvalidChoice () {
 }
  void Intro ()
 {
-    Console.WriteLine(" Cameron Kingsley ITSE 1430 9/5/2023");
-    Console.WriteLine("You awaken on the floor of a dark room.");
-    Console.WriteLine("As your vision begins to clear, yout notice");
-    Console.WriteLine("there are two flickering lights on the wall,");
-    Console.WriteLine("in two corners. surrounded by 3 large wooden ");
-    Console.WriteLine("doorways.and a mirror on the 4th side of the");
-    Console.WriteLine(" room.There is nothing else in the room");
+    Console.WriteLine("         Cameron Kingsley ITSE 1430 9/5/2023");
+    Console.WriteLine("--------------------------------------------------------");
+    Console.WriteLine("       You awaken on the floor of a dark room.");
+    Console.WriteLine("     As your vision begins to clear, yout notice");
+    Console.WriteLine("     there are two flickering lights on the wall,");
+    Console.WriteLine("    in two corners, surrounded by 3 large wooden ");
+    Console.WriteLine("   doorways.There is a mirror on the 4th side of the");
+    Console.WriteLine("  room. Upon The ground is a compass rose painted in red.");
+    Console.WriteLine("           The mirror is on the south wall");
+    Console.WriteLine("          There is nothing else in the room");
 
 }
 int GameLoop ()
@@ -64,7 +98,8 @@ int GameLoop ()
         Console.WriteLine("You selected west.");
         roomNumber= roomNumber-1;
         calcValidation();
-        Console.WriteLine("room "+ roomNumber);
+        displayCoordinate();
+        Console.WriteLine("room "+ adjustedlocation);
         return roomNumber;
 
         case 's':
@@ -72,7 +107,8 @@ int GameLoop ()
         Console.WriteLine("You selected south.");
         roomNumber-=4;
         calcValidation();
-        Console.WriteLine("room "+roomNumber);
+        displayCoordinate();
+        Console.WriteLine("room "+ adjustedlocation);
         return roomNumber;
         case 'e':
         case 'E':
@@ -80,20 +116,22 @@ int GameLoop ()
         Console.WriteLine("You selected east.");
         roomNumber +=1;
         calcValidation();
-        Console.WriteLine("room "+roomNumber);
+        displayCoordinate();
+        Console.WriteLine("room "+ adjustedlocation);
         return roomNumber;
         case 'n':
         case 'N':
         Console.WriteLine("You selected north.");
         roomNumber +=4;
         calcValidation();
-        Console.WriteLine("room "+roomNumber);
+        displayCoordinate();
+        Console.WriteLine("room "+ adjustedlocation);
         return roomNumber;
         case 'i':
         case 'I':
         Console.WriteLine("You decided to investigate.");
 
-        Console.WriteLine("room "+roomNumber);
+        Console.WriteLine("room "+ adjustedlocation);
         return roomNumber;
         case 'q':
         case 'Q':
@@ -102,7 +140,7 @@ int GameLoop ()
         if (value == 'y'||value =='Y' )
         {
 
-            return 0;
+           return 0;
 
         } else
         {
@@ -111,7 +149,7 @@ int GameLoop ()
         }
         default:
         Console.WriteLine("Select a direction: north, south, east, west. ");
-        Console.WriteLine("room "+roomNumber);
+        Console.WriteLine("room "+ adjustedlocation);
         return roomNumber;
 
     };
@@ -119,15 +157,36 @@ int GameLoop ()
 }
  void RoomOne ()
 {
-    Console.WriteLine("room one");
+    Console.WriteLine("--------------------------------------------------------");
+    Console.WriteLine("       There is one flickering light in the corner,");
+    Console.WriteLine("      Upon The ground is a compass rose painted in red.");
+    Console.WriteLine("        There is a mirror on the south side of the");
+    Console.WriteLine("       room. Two doors lie to the north and the east.");
+    Console.WriteLine("          Upon the west wall is a mounted deer head.");
+    Console.WriteLine("          There is nothing else in the room ");
+    Console.WriteLine("--------------------------------------------------------");
 }
  void RoomTwo ()
 {
-    Console.WriteLine("test success");
+    Console.WriteLine("--------------------------------------------------------");
+    Console.WriteLine("     There are two flickering lights on the wall,");
+    Console.WriteLine("    in two corners, surrounded by 3 large wooden ");
+    Console.WriteLine("   doorways.There is a mirror on the 4th side of the");
+    Console.WriteLine("  room. Upon The ground is a compass rose painted in red.");
+    Console.WriteLine("           The mirror is on the south wall.");
+    Console.WriteLine("          There is nothing else in the room");
+    Console.WriteLine("--------------------------------------------------------");
 }
- void RoomThree ()
+void RoomThree ()
 {
-    Console.WriteLine("room three");
+    Console.WriteLine("--------------------------------------------------------");
+    Console.WriteLine("       There is one flickering light in the corner,");
+    Console.WriteLine("      Upon The ground is a compass rose painted in red.");
+    Console.WriteLine("        There is a mirror on the south side of the");
+    Console.WriteLine("       room. Two doors lie to the north and the west.");
+    Console.WriteLine("          Upon the east wall is a stuffed bear statue.");
+    Console.WriteLine("          There is nothing else in the room ");
+    Console.WriteLine("--------------------------------------------------------");
 }
  void RoomFour ()
 {
@@ -135,7 +194,12 @@ int GameLoop ()
 }
  void RoomFive ()
 {
-    Console.WriteLine(" room five");
+    Console.WriteLine("--------------------------------------------------------");
+    Console.WriteLine("         You are surrounded by 4 doorways.");
+    Console.WriteLine("     All four corners of the room have a torch.");
+    Console.WriteLine("   In the center of the room lies a table covered");
+    Console.WriteLine(" in taxidermy tools. Above is a large windowed ceiling.");
+    Console.WriteLine("--------------------------------------------------------");
 }
  void RoomSix ()
 {
@@ -143,7 +207,14 @@ int GameLoop ()
 }
  void RoomSeven ()
 {
-    Console.WriteLine("room seven");
+    Console.WriteLine("--------------------------------------------------------");
+    Console.WriteLine("      Upon The ground is a compass rose painted in red.");
+    Console.WriteLine("       There is one flickering light in the south-east corner,");
+    Console.WriteLine("        There is a mirror on the south side of the");
+    Console.WriteLine("       room. Two doors lie to the south and the east.");
+    Console.WriteLine("          Upon the west wall is a mounted deer head.");
+    Console.WriteLine("          There is nothing else in the room ");
+    Console.WriteLine("--------------------------------------------------------");
 }
  void RoomEight ()
 {
@@ -163,7 +234,7 @@ while (v)
    
 
 
-    switch (roomNumber)
+    switch (adjustedlocation)
     {
         case 1:
         RoomOne();
